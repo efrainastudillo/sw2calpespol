6d28ee5abe5e45ef6dfff7755c1ade4f186c0bec
Brick Reyes <brickrey@me.com>
2012-07-27 23:54:00 -0500

    trabajando en el modulo grupo

--- calpespol/apps/frontend/modules/Grupo/templates/indexSuccess.php
+++ calpespol/apps/frontend/modules/Grupo/templates/indexSuccess.php
@@ -1,27 +1,17 @@
 <?php slot('logo') ?>
     <?php echo image_tag('/images/grupo.png', 'alt_title=Grupos') ?>
 <?php end_slot(); ?>
-
 <?php slot('title') ?>
     <?php echo "Grupos" ?>
 <?php end_slot(); ?>
-<?php use_stylesheet('grupos.css') ?>
-<?php use_javascript('grupo.js') ?>
-Materia: <?php echo $sf_user->getMateriaActual() ?><br/>
-Paralelo: <?php echo $sf_user->getParaleloActual() ?><br/>
-Rol Usuario: <?php echo $sf_user->getRol() ?><br/>
-Es estudiante?: <?php echo $sf_user->isEstudiante() ?><br/>
-<input id="grupo_button_new" type="button" value="      Editar" onClick="window.open('Grupo/new')" style="float: right"/>
-<?php if (!$sf_user->isEstudiante()) echo "<input id=\"grupo_button_edit\" type=\"button\" value=\"      Editar\" onClick=\"window.open('Grupo/edit')\" style=\"float: right\"/>" ?>
-<?php if (!$sf_user->isEstudiante()) echo "<input id=\"grupo_button_delete\" type=\"button\" value=\"      Eliminar\" onClick=\"window.open('Grupo/delete')\" style=\"float: right\"/>" ?>
+
+<input id="grupo_button_new" type="button" value="      Nuevo" onClick="window.open('Grupo/new','_self')" style="float: right"/>
+<?php if (strcasecmp($rol,"Ayudante")||strcasecmp($rol,"Profesor")) echo "<input id=\"grupo_button_edit\" type=\"button\" value=\"      Editar\" onClick=\"window.open('Grupo/edit')\" style=\"float: right\"/>" ?>
+<?php if (strcasecmp($rol,"Ayudante")||strcasecmp($rol,"Profesor")) echo "<input id=\"grupo_button_delete\" type=\"button\" value=\"      Eliminar\" onClick=\"window.open('Grupo/delete')\" style=\"float: right\"/>" ?>
 <div style="clear: both"></div>
+<input type="hidden" id="grupo_var_curso" value="<?php echo $id_curso ?>" />
 <table id="grupo_consulta">
-    <tr>
-        <th>Apellidos</th>
-        <th>Nombres</th>
-        <th>Grupo</th>
-        <th>Correo</th>
-    </tr>
+    <tr><th>Apellidos</th><th>Nombres</th><th>Grupo</th><th>Correo</th></tr>
     <?php foreach ($lista as $objeto): ?>
     <tr>
         <td><?php echo $objeto->getUsuario()->getApellido() ?></td>
@@ -31,12 +21,3 @@ Es estudiante?: <?php echo $sf_user->isEstudiante() ?><br/>
     </tr>
     <?php endforeach; ?>
 </table>
-
-<?php echo 
-        "<script type='text/javascript'>
-            jQuery(document).ready(function($)
-            {
-                $('.tabla').tableScroll({width:950, height:200});
-            });
-        </script>"
-        ?>
